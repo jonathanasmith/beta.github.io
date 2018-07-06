e4d7ed01148e89d24c8f19a45f8235b259dd4e69
jonathanasmith <smithjas@gmail.com>
2018-07-06 11:26:17 +0100

    and
    and

---
+++ JS/main.js
@@ -0,0 +1,39 @@
+
+// Initialise Parse
+
+
+Parse.initialize(
+  "bxr71QksTuJsPlLRodiehCBmfPtrKIzSsuu2YtId",
+  "AMzZx8qW1pw9STOOjf625wkDau1vzddW0svKd7Dq"
+);
+Parse.serverURL = 'https://parseapi.back4app.com';
+
+// Functions
+
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
+
+// Remaining script
+
+
+Parse.Cloud.run('testFunction', {
+
+    success: function(result) {
+      alert("success")
+    },
+
+    error: function(error) {
+      alert("error")
+    }
+    
+  });
+
